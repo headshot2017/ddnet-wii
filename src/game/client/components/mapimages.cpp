2bae8e64058338a0cda0e6b38418a6d8432ca72a
headshot2017 <>
2024-05-11 20:01:02 -0400

    hex

--- src/game/client/components/mapimages.cpp
+++ src/game/client/components/mapimages.cpp
@@ -40,7 +40,7 @@ void CMapImages::OnMapLoad()
 			char Buf[256];
 			char *pName = (char *)pMap->GetData(pImg->m_ImageName);
 			str_format(Buf, sizeof(Buf), "mapres/%s.png", pName);
-			dbg_msg("img", "1 '%s' '%d'", pName, pImg->m_ImageName);
+			dbg_msg("img", "1 0x%08x '%s' '%d'", pName, pName, pImg->m_ImageName);
 			m_aTextures[i] = Graphics()->LoadTexture(Buf, IStorage::TYPE_ALL, CImageInfo::FORMAT_AUTO, 0);
 		}
 		else
@@ -76,7 +76,7 @@ void CMapImages::LoadBackground(class IMap *pMap)
 			char Buf[256];
 			char *pName = (char *)pMap->GetData(pImg->m_ImageName);
 			str_format(Buf, sizeof(Buf), "mapres/%s.png", pName);
-			dbg_msg("img", "2 '%s' '%d'", pName, pImg->m_ImageName);
+			dbg_msg("img", "2 0x%08x '%s' '%d'", pName, pName, pImg->m_ImageName);
 			m_aTextures[i] = Graphics()->LoadTexture(Buf, IStorage::TYPE_ALL, CImageInfo::FORMAT_AUTO, 0);
 		}
 		else
